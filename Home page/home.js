ef8934dda3356d4dc3b51111da4e1faf8e3a6dec
Sanket Survase <sanketsurvase123@gmail.com>
2026-09-23 16:00:06 +0530

    Update complaint management pages

--- Home page/home.js
+++ Home page/home.js
@@ -50,7 +50,7 @@ document.addEventListener("DOMContentLoaded", function () {
             else if (title === "Check Complaint Status") {
 
                 window.location.href =
-                    "../Status/Status.html";
+                    "../status/Status.html";
 
             }
 
